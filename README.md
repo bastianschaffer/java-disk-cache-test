d1fc387275617e1e6f6827448305777b31c3e38f
Bastian Schaffer <bastianschaffer.it@gmail.com>
2022-12-03 13:34:47 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # java-disk-cache-test
+
+This is a test implementation of the [Apache Commons Java Caching System](https://commons.apache.org/proper/commons-jcs/)
+
+The cache is in the [caching.ccf](src/main/resources/cache.ccf) file configured to cache 0 objects on memory and 100 on the disk. To check if the data is really on the disk, you can set "fillCache" to false and run the test again, but this only works if the test ran at least once with "fillCache" set to true.
+
+## helpful links
+https://commons.apache.org/proper/commons-jcs/IndexedDiskAuxCache.html
+https://commons.apache.org/proper/commons-jcs/IndexedDiskCacheProperties.html
+
+
+
---
+++ src/test/java/TestDiskCache.java
@@ -0,0 +1,89 @@
+import org.apache.commons.jcs3.JCS;
+import org.apache.commons.jcs3.access.CacheAccess;
+import org.apache.commons.jcs3.access.exception.CacheException;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDiskCache {
+
+    private CacheAccess<String, Set<String>> jcsCache = null;
+    private final int cachedCriteriaAmount = 100;
+    private final int idsPerCriterion = 50;
+    private final boolean fillCache = false;
+    private HashMap<String, Set<String>> testData;
+
+    @Test
+    public void mainTest() throws InterruptedException {
+        testData = generateData();
+
+        try{
+            jcsCache = JCS.getInstance("default");
+        }catch(CacheException e){
+            System.out.printf("Problem initializing cache: %s%n", e.getMessage() );
+        }
+
+        if(fillCache){
+            putDataToJcs();
+            TimeUnit.SECONDS.sleep(1); //without this sleep, the cache would for some reason find all values, although they aren't (/shouldn't be) in the cache
+        }
+
+        String keyTemplate = "someKey-";
+        float totalTime = 0;
+        for(int i = 0; i < cachedCriteriaAmount; i++){
+            String key = keyTemplate + i;
+            Set<String> valueSet = testData.get(key);
+
+            long startTime = System.nanoTime();
+            Set<String> foundValues = jcsGet(key);
+            long endTime = System.nanoTime();
+
+            assertEquals(valueSet, foundValues);
+             totalTime += (float)(endTime - startTime) / 1000000.0;
+        }
+        System.out.printf("It took on average %fms to get an entry%n", totalTime / cachedCriteriaAmount);
+    }
+
+    private void putDataToJcs(){
+        String keyTemplate = "someKey-";
+        for(int i = 0; i < cachedCriteriaAmount; i++){
+            String key = keyTemplate + i;
+            Set<String> valueSet = testData.get(key);
+
+            jcsPut(key, valueSet);
+        }
+    }
+
+
+    private HashMap<String, Set<String>> generateData(){
+        HashMap<String, Set<String>> data = new HashMap<>();
+        String keyTemplate = "someKey-";
+        for(int i = 0; i < cachedCriteriaAmount; i++){
+            String key = keyTemplate + i;
+            String valueTemplate = "val-";
+            Set<String> valueSet = new HashSet<>();
+            for(int j = 0; j < idsPerCriterion; j++){
+                valueSet.add(valueTemplate + (i) );
+            }
+
+            data.put(key, valueSet);
+        }
+        return data;
+    }
+
+    public void jcsPut(String key, Set<String> values){
+        try
+        {
+            jcsCache.put(key, values);
+        }
+        catch (CacheException e)
+        {
+            System.out.printf("Problem putting entry in the cache, for key %s%n%s%n", key, e.getMessage());
+        }
+    }
+
+    public Set<String> jcsGet(String key){
+        return jcsCache.get(key);
+    }
+}
